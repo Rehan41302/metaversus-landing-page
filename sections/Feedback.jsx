db94acf99dd49a660535581c884fb0769ffb89b2
Rehan41302 <saeedrehan3@gmail.com>
2023-05-29 15:01:02 +0500

    feat: implemented footerb section

--- sections/Feedback.jsx
+++ sections/Feedback.jsx
@@ -3,7 +3,6 @@
 import { motion } from "framer-motion";
 
 import styles from "../styles";
-import { TitleText, TypingText } from "../components";
 import { fadeIn, staggerContainer, zoomIn } from "../utils/motion";
 
 const Feedback = () => (
